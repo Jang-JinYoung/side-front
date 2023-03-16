00c607e2b5dd9292fdb17059c387ba67f0974997
장진영 <jinyoung_8965@naver.com>
2023-03-16 17:33:44 +0900

    게시판 css 수정

--- src/pages/BoardPage.tsx
+++ src/pages/BoardPage.tsx
@@ -1,31 +1,88 @@
-import React from "react";
-import { useNavigate } from "react-router-dom";
+import React, { useCallback, useEffect, useState } from "react";
+import { useLocation, useNavigate, useParams } from "react-router-dom";
+import _ from "lodash";
+import { Header } from "@components/";
 import data from "./data.json";
+import Table from "react-bootstrap/Table";
+
+const pagination = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 const BoardPage = () => {
   const navigate = useNavigate();
+  const page = new URLSearchParams(useLocation().search).get("page") ?? 1;
+
+  const [inputValue, setInputValue] = useState("");
+
+  // 500ms 간격으로 onChange 이벤트를 호출하는 함수를 생성
+  const throttledOnChange = useCallback(
+    _.throttle((e) => {
+      setInputValue(e.target.value);
+    }, 500),
+    []
+  );
+
+  useEffect(() => {
+    console.log("page", page);
+
+    return () => {
+      console.log("clean up");
+    };
+  }, []);
 
   return (
-    <div>
-      <table className="board">
-        <thead>
-          <tr>
-            <th>번호</th>
-            <th>작성자</th>
-            <th>제목</th>
-          </tr>
-        </thead>
-        <tbody>
-          {data.products.map((v, i) => (
-            <tr key={v.id} onClick={() => navigate(`/board/${v.id}`)}>
-              <td>{v.id}</td>
-              <td>{v.description}</td>
-              <td>{v.name}</td>
+    <>
+      <Header name="jjy" />
+      <div className="board_wrap">
+        <Table striped bordered hover className="board">
+          <thead>
+            <tr>
+              <th>번호</th>
+              <th>제목</th>
+              <th>작성자</th>
+              <th>작성날짜</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
+          </thead>
+          <tbody>
+            {data.products.map((v, i) => (
+              <tr key={v.id} onClick={() => navigate(`/board/${v.id}`)}>
+                <td>{v.id}</td>
+                <td>{v.description}</td>
+                <td>{v.name}</td>
+                <td>
+                  {new Intl.DateTimeFormat("ko", {
+                    timeStyle: "medium",
+                    dateStyle: "medium",
+                  }).format(new Date())}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+        <div>
+          <ul className="pagination">
+            {pagination.map((v) => (
+              <li
+                key={v}
+                onClick={() => navigate(`/board?page=${v}`)}
+                className={page === `${v}` ? "on" : ""}
+              >
+                {v}
+              </li>
+            ))}
+            <li>&gt;</li>
+          </ul>
+        </div>
+        <div className="search">
+          <label htmlFor="username">검색 </label>
+          <input
+            type="text"
+            name="search"
+            onChange={(e) => throttledOnChange(e)}
+          />
+          <button onClick={() => {}}>안녕</button>
+        </div>
+      </div>
+    </>
   );
 };
 
