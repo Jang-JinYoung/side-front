22252980fef14403d4c84b2cae2c15ded9b702a9
장진영 <jyjang@openit.co.kr>
2023-08-16 17:35:44 +0900

    소스정리 및 컴포넌트 추가

---
+++ src/elements/pages/MessagePage.tsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import Header from '@components/Header';
+import data from '../../data/receiveMessage.json';
+
+interface IMsg {
+  msgSrno: number;
+  msg: string;
+}
+
+interface IReceiveMsg extends IMsg {}
+
+const MessagePage = () => {
+  const { result } = data;
+
+  return (
+    <>
+      <Header />
+      <div className="message_wrap">
+        <div
+          style={{
+            position: 'relative',
+            float: 'left',
+            width: '250px',
+            height: '100%',
+            borderRight: '1px solid black',
+            // left: '10%',
+            verticalAlign: 'center',
+          }}
+        >
+          <ul className="pt-15">
+            <li>받은메시지</li>
+            <li>보낸메시지</li>
+          </ul>
+        </div>
+        <div style={{ marginLeft: '300px' }}>
+          <table className="message ta-c">
+            <thead>
+              <tr className="ta-c">
+                <th style={{ width: '50px' }}>
+                  <input type="checkbox"></input>
+                </th>
+                <th style={{ width: '75px' }}>보낸사람</th>
+                <th style={{ width: '200px' }}>내용</th>
+                <th style={{ width: '150px' }}>날짜</th>
+              </tr>
+            </thead>
+            <tbody>
+              {result.map((item: IMsg) => (
+                <tr key={item.msgSrno}>
+                  <td>{item.msgSrno}</td>
+                  <td>{item.sender}</td>
+                  <td>{item.msg}</td>
+                  <td>{item.receiveTm}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default MessagePage;
