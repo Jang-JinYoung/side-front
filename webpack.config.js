2013c46a9ad9888738a4a12d9063242159f514d6
jyjang <jyjang@openit.co.kr>
2025-04-03 17:22:43 +0900

    feat : local env 환경 추가

--- webpack.config.js
+++ webpack.config.js
@@ -3,101 +3,99 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const path = require('path');
 const webpack = require('webpack');
 const {
-  VanillaExtractPlugin
+    VanillaExtractPlugin
 } = require('@vanilla-extract/webpack-plugin');
 
+const env = require('./env/local.env.js');
+console.log("DEVTOOL >>> ", env.DEVTOOL);
 
-module.exports = (env, argv) => {
-  const prod = argv.mode === 'production';
-
-  return {
-    mode: prod ? 'production' : 'development',
+module.exports = {
+    mode: process.env.NODE_ENV,
     devtool: false,
     entry: './src/index.tsx',
     output: {
-      publicPath: "/",
+        publicPath: "/",
 
-      // 23.8.24 이게 뭘까요
-      // path: path.join(__dirname, '/dist'),
-      // filename: '[name].js',
+        // 23.8.24 이게 뭘까요
+        // path: path.join(__dirname, '/dist'),
+        // filename: '[name].js',
     },
     devServer: {
-      port: 3000,
-      hot: true,
+        port: 3000,
+        hot: true,
     },
     resolve: {
-      extensions: ['.js', '.jsx', '.ts', '.tsx'],
-      alias: {
-        '@public': path.resolve(__dirname, 'public'),
-        '@atoms': path.resolve(__dirname, 'src/elements/atoms/'),
-        '@components': path.resolve(__dirname, 'src/elements/components/'),
-        '@pages': path.resolve(__dirname, 'src/elements/pages/'),
-        '@store': path.resolve(__dirname, 'src/store/'),
-        '@service': path.resolve(__dirname, 'src/service/'),
-        '@lib': path.resolve(__dirname, 'src/lib/'),
-        '@interface': path.resolve(__dirname, 'src/interface/'),
-        '@api': path.resolve(__dirname, 'src/service/api/'),
-      },
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+        alias: {
+            '@public': path.resolve(__dirname, 'public'),
+            '@atoms': path.resolve(__dirname, 'src/elements/atoms/'),
+            '@components': path.resolve(__dirname, 'src/elements/components/'),
+            '@pages': path.resolve(__dirname, 'src/elements/pages/'),
+            '@store': path.resolve(__dirname, 'src/store/'),
+            '@service': path.resolve(__dirname, 'src/service/'),
+            '@lib': path.resolve(__dirname, 'src/lib/'),
+            '@interface': path.resolve(__dirname, 'src/interface/'),
+            '@api': path.resolve(__dirname, 'src/service/api/'),
+        },
     },
     module: {
-      rules: [
-        {
-          test: /\.tsx?$/,
-          exclude: /node_modules/,
-          use: ['babel-loader', 'ts-loader'],
-        },
-        {
-          test: /\.css$/i,
-          include: path.resolve(__dirname, 'src'),
-          use: ['style-loader', 'css-loader', 'postcss-loader'],
-        },
-        {
-          test: /\.(png|jpg|gif)$/,
-          use: [
+        rules: [
             {
-              loader: 'file-loader',
-              options: {
-                name: '[name].[ext]',
-                outputPath: 'images/',
-                publicPath: '/images/',
-              },
+                test: /\.tsx?$/,
+                exclude: /node_modules/,
+                use: ['babel-loader', 'ts-loader'],
             },
-          ],
-        },
-      ],
+            {
+                test: /\.css$/i,
+                include: path.resolve(__dirname, 'src'),
+                use: ['style-loader', 'css-loader', 'postcss-loader'],
+            },
+            {
+                test: /\.(png|jpg|gif)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].[ext]',
+                            outputPath: 'images/',
+                            publicPath: '/images/',
+                        },
+                    },
+                ],
+            },
+        ],
     },
     devServer: {
-      static: {
-        directory: path.resolve(__dirname, 'public'),
-      },
-      compress: true,
-      port: 3000,
-      hot: true,
-      open: true,
-      historyApiFallback: true,
+        static: {
+            directory: path.resolve(__dirname, 'public'),
+        },
+        compress: true,
+        port: 3000,
+        hot: true,
+        // 새로운 창 실행 여부
+        // open: true,
+        historyApiFallback: true,
     },
     plugins: [
-      
-      new webpack.ProvidePlugin({
-        React: 'react',
-      }),
-      // new Dotenv(),
-      new HtmlWebpackPlugin({
-        template: './public/index.html',
-        minify:
-          process.env.NODE_ENV === 'production'
-            ? {
-                collapseWhitespace: true, // 빈칸 제거
-                removeComments: true, // 주석 제거
-              }
-            : false,
-      }),
-      new webpack.DefinePlugin({
-        'process.env': JSON.stringify(process.env),
-      }),
-      new CleanWebpackPlugin(),
-      new VanillaExtractPlugin(),
+        new webpack.DefinePlugin({
+            'process.env': env
+        }),
+        new webpack.ProvidePlugin({
+            React: 'react',
+        }),
+        // new Dotenv(),
+        new HtmlWebpackPlugin({
+            template: './public/index.html',
+            minify:
+                process.env.NODE_ENV === 'production'
+                    ? {
+                        collapseWhitespace: true, // 빈칸 제거
+                        removeComments: true, // 주석 제거
+                    }
+                    : false,
+        }),
+        new CleanWebpackPlugin(),
+        new VanillaExtractPlugin(),
     ],
-    
-  };
+
 };
