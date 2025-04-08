31b2d983e406269877a4c3ee5d8c533a25f8de8d
jyjang <jyjang@openit.co.kr>
2025-04-08 17:20:52 +0900

    feat : 문구수정

--- src/component/SummaryInformation.tsx
+++ src/component/SummaryInformation.tsx
@@ -1,18 +1,17 @@
 const SummaryInformation = () => {
     return (
         <div className="mb-6">
-            <h2 className="text-lg font-bold mb-4">요약 정보</h2>
             <div className="mb-2">
-                <span className="block text-gray-600">총 입금</span>
+                <span className="block text-gray-600">수입</span>
                 <span className="text-green-500 font-bold text-xl">+2,500,000 원</span>
             </div>
             <div className="mb-2">
-                <span className="block text-gray-600">총 지출</span>
+                <span className="block text-gray-600">지출</span>
                 <span className="text-red-500 font-bold text-xl">-1,200,000 원</span>
             </div>
-            <div>
+            <div className="mb-2">
                 <span className="block text-gray-600">잔액</span>
-                <span className="text-blue-500 font-bold text-xl">+1,300,000 원</span>
+                <span className="text-gray-500 font-bold text-xl">+1,300,000 원</span>
             </div>
         </div>
     );
