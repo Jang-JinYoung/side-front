01698f3d9961f8b1c72a422f14e39ee8996c6f38
jyjang <jyjang@openit.co.kr>
2025-04-08 10:10:24 +0900

    feat : 슬라이드판넬 스크롤 추가

--- src/atom/SlidePanel.tsx
+++ src/atom/SlidePanel.tsx
@@ -6,19 +6,18 @@ import { TRecordTransaction } from "@type/RecordTransaction";
 interface IProps {
     isOpen: boolean,
     setOpen: TonClick,
-    data?: any,
+    data?: TRecordTransaction[] | null,
 };
 
 const SlidingPanel = ({ isOpen, setOpen, data }: IProps) => {
 
-    console.log(data);
 
     return (
         <div className="relative">
 
             {/* 슬라이드 패널 */}
             <div
-                className={`fixed top-0 right-0 h-full w-[20%] bg-gray-800 text-white transform transition-transform duration-500 ease-in-out ${isOpen ? "translate-x-0" : "translate-x-full"}`}
+                className={`fixed top-0 right-0 h-full w-[20%] bg-gray-800 text-white transform transition-transform duration-500 ease-in-out overflow-y-auto ${isOpen ? "translate-x-0" : "translate-x-full"} `}
             >
                 <Button
                     className="absolute top-4 right-4 bg-red-500"
@@ -26,13 +25,44 @@ const SlidingPanel = ({ isOpen, setOpen, data }: IProps) => {
                     text="X"
                 />
 
-                <div className="p-4">
-                    <h2 className="text-lg font-bold">슬라이드 패널</h2>
-                    <p>여기에 내용을 추가하세요.</p>
+                <div className="overflow-y-scroll h-full  p-4 mt-15">
                     {
-                        data && data.map((t: any) => <div>{t.amount}</div>)
+                        data && data.map((t: TRecordTransaction) =>
+                            <Card data={t} />
+                        )
                     }
                 </div>
+            </div>
+        </div>
+    );
+};
+const Card = ({ data }: {data:TRecordTransaction}) => {
+
+    const { type, category, description, amount } = data;
+    
+    return (
+        <div className="flex items-center justify-between p-4 bg-gray-100 border rounded-lg shadow-md mb-5">
+            {/* 좌측 영역 */}
+            <div>
+                {/* 지출/입금 카테고리 */}
+                <span
+                    className={`block text-sm font-bold ${type === "지출" ? "text-red-500" : "text-green-500"
+                        }`}
+                >
+                    {type} - {category}
+                </span>
+                {/* 설명 */}
+                <p className="text-gray-700 text-sm mt-1">{description}</p>
+            </div>
+
+            {/* 우측 금액 */}
+            <div className="text-right">
+                <span
+                    className={`text-lg font-bold ${type === "지출" ? "text-red-500" : "text-green-500"
+                        }`}
+                >
+                    {amount.toLocaleString()} 원
+                </span>
             </div>
         </div>
     );
