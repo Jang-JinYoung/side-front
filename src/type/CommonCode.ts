1a8e7e8e346af95a7395ccbc25d1c3ffca5fd121
장진영 <jinyoung_8965@naver.com>
2025-04-21 19:03:33 +0900

    feat : 공통 코드 타입 추가

---
+++ src/type/CommonCode.ts
@@ -0,0 +1,19 @@
+import { z } from "zod";
+
+/**
+ * 기본
+ */
+export const CommonCodeSchema = z.object({
+    // 코드번호
+    code: z.string().max(8), 
+    // 부모코드번호
+    parentCode: z.string().max(8).or(z.null()),
+    // 코드 고유명
+    codrKorName: z.string().max(8),
+    // 코드 공통명
+    codeName: z.string(),
+    // 내용
+    description: z.string().max(8),
+});
+
+export type TCommonCode = z.infer<typeof CommonCodeSchema>;
