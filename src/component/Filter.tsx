f38eb4097813e179d81a2df0f0990008633150a2
장진영 <jinyoung_8965@naver.com>
2025-04-22 16:28:39 +0900

    feat : 요약컴포넌트 삭제 및 컴포넌트 수정

--- src/component/Filter.tsx
+++ src/component/Filter.tsx
@@ -1,21 +1,55 @@
-import SummaryInformation from "./SummaryInformation";
+import { TCommonCode } from "@type/CommonCode";
 
-const Filter = () => {
+const Filter = ({ statistics, codes }: { statistics: any, codes: any }) => {
+
     return (
         <div className="w-1/5 bg-gray-50 p-4 border-r shadow-md">
             {/* 총입금/총지출/잔액 */}
-            <SummaryInformation />
+            <SummaryInformation statistics={statistics} />
 
             {/* 필터 */}
-            <div>
-                <select
+            <div className="mb-4">
+                <label className="block text-gray-700 text-sm font-bold mb-2">
+                    유형
+                </label>
+                <div className="flex space-x-4">
+                    <label className="inline-flex items-center">
+                        <input
+                            type="radio"
+                            name="transactionCode"
+                            value={"10000000"}
+                            // checked={formData.transactionCode === code.code}
+                            // onChange={onChange}
+                            className="form-radio"
+                        />
+                        <span className="ml-2">전체</span>
+                    </label>
+                    {
+                        codes &&
+                        codes.map((code: TCommonCode) =>
+                            <label className="inline-flex items-center">
+                                <input
+                                    type="radio"
+                                    name="transactionCode"
+                                    value={code.code}
+                                    // checked={formData.transactionCode === code.code}
+                                    // onChange={onChange}
+                                    className="form-radio"
+                                />
+                                <span className="ml-2">{code.codeKorName}</span>
+                            </label>
+                        )
+                    }
+                </div>
+
+                {/* <select
                     className="w-full p-2 border rounded-lg mb-4"
                     onChange={(e) => console.log(e.target.value)}
                 >
                     <option value="">전체 보기</option>
-                    <option value="income">입금만 보기</option>
-                    <option value="expense">지출만 보기</option>
-                </select>
+                    <option value="income">입금</option>
+                    <option value="expense">지출</option>
+                </select> */}
 
                 {/* 날짜 필터 */}
                 <label
@@ -36,5 +70,25 @@ const Filter = () => {
         </div>
     );
 }
+
+const SummaryInformation = ({ statistics }: { statistics: any }) => {
+    return (
+        <div className="mb-6">
+            <div className="mb-2">
+                <span className="block text-gray-600">수입</span>
+                <span className="text-green-500 font-bold text-xl">+{statistics?.income} 원</span>
+            </div>
+            <div className="mb-2">
+                <span className="block text-gray-600">지출</span>
+                <span className="text-red-500 font-bold text-xl">-{statistics?.expense} 원</span>
+            </div>
+            <div className="mb-2">
+                <span className="block text-gray-600">잔액</span>
+                <span className="text-gray-500 font-bold text-xl">{statistics?.balance} 원</span>
+            </div>
+        </div>
+    );
+};
+
 
 export default Filter;
