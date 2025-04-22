0e8a0b3f80b665d7a2471e3c638e489c52c6ad2c
장진영 <jinyoung_8965@naver.com>
2025-04-22 16:29:19 +0900

    feat: 오타수정

--- src/type/CommonCode.ts
+++ src/type/CommonCode.ts
@@ -9,7 +9,7 @@ export const CommonCodeSchema = z.object({
     // 부모코드번호
     parentCode: z.string().max(8).or(z.null()),
     // 코드 고유명
-    codrKorName: z.string().max(8),
+    codeKorName: z.string(),
     // 코드 공통명
     codeName: z.string(),
     // 내용
